311f571b34526c66f038b91549ecc979f4d248f3
chris <chunghao777@gmail.com>
2023-12-06 21:09:42 +0800

    cmake build

---
+++ build/CMakeFiles/mydev.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mydev.dir/main.c.o"
+  "CMakeFiles/mydev.dir/main.c.o.d"
+  "mydev"
+  "mydev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mydev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mydev.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mydev.
+# This may be replaced when dependencies are built.
